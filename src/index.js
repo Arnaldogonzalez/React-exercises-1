a218dfeca7a0c4c7b6355e069e0db3212f16c9a9
Arnaldogonzalez <agonzalez@fvi.edu>
2017-10-10 18:27:32 -0400

    first exercises

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function NewBook(){
+  return (
+    <div className="Arnaldo">
+      <div className="title">
+        The Title
+      </div>
+      <div className="author">
+        The Author
+      </div>
+      <ul className="stats">
+        <li className="rating">
+        5 starts
+        </li>
+        <li className="isbn">
+        12-345678-910
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <NewBook />,
+  document.querySelector('#root')
+);
